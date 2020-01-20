329350b380f9ca6623bb235b503b22f9679c214b
Aricoin <35907563+AricoinCurrency@users.noreply.github.com>
2020-01-20 17:45:10 +0000

    Checkpoints updated

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -20,7 +20,7 @@ namespace Checkpoints
 	// Reference - https://chainz.cryptoid.info/ari/block.dws?1.htm
     static MapCheckpoints mapCheckpoints =
             boost::assign::map_list_of
-        (     0, uint256("0x6df743739be39a62277fb5f95dd9bb25c68ac03fd1293183f000315bd1c87e0f"))
+    (  0, uint256("0x6df743739be39a62277fb5f95dd9bb25c68ac03fd1293183f000315bd1c87e0f"))
     (  1000, uint256("0xb471022e6ecb336a3d0327f00ef417fa1bc79652299f3015f553f6c0086861ac"))
     ( 10000, uint256("0x8bbdc0e43fffeb9dfb2fdd2b590236e3a5693c8a125a689263bf84244899d6e5"))
     ( 20000, uint256("0x27a654d36a38c84f2458f6f6945016a64b7742b805868ea27a87d22635c9a743"))
